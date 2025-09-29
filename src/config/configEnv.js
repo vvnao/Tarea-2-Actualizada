3450ca03f8a05ed7fb8be1016b0f9deb7f0e248d
Valentina Johana Cecilia Martínez Hitschfeld <valentina.martinez2302@alumnos.ubiobio.cl>
2025-09-28 21:49:39 -0300

    Agregué cambios para poder ocuparlo en el servidor ubb

--- src/config/configEnv.js
+++ src/config/configEnv.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const HOST = process.env.DB_HOST || process.env.HOST || "localhost";
 export const PORT = process.env.PORT || 3000;
-export const DB_PORT = process.env.DB_PORT || 5432;
+export const DB_PORT = process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432;
 export const DB_USERNAME = process.env.DB_USERNAME;
 export const PASSWORD = process.env.DB_PASSWORD;
 export const DATABASE = process.env.DATABASE;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
-import { AppDataSource, connectDB } from "./config/configDB.js";
+import { connectDB } from "./config/configDb.js";
 import { routerApi } from "./routes/index.routes.js";
+import { HOST, PORT } from "./config/configEnv.js"; 
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+
 // Ruta principal de bienvenida
 app.get("/", (req, res) => {
-  res.send("¡Bienvenido a mi API REST con TypeORM!");
+  res.send("Bienvenido a mi API REST con TypeORM!");
 });
 
 // Inicializa la conexión a la base de datos
 connectDB()
   .then(() => {
-    // Carga todas las rutas de la aplicación
     routerApi(app);
 
     // Levanta el servidor Express
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor iniciado en http://localhost:${PORT}`);
+    app.listen(PORT, HOST, () => {
+      console.log(`Servidor iniciado en http://${HOST}:${PORT}`);
     });
   })
   .catch((error) => {
